24aaf78400c31b8e0a4abbedf0304d8047ec2fce
sachin-sinha01 <68593748+sachin-sinha01@users.noreply.github.com>
2022-01-28 21:40:48 +0530

    Update misc.py

--- Yuriko/modules/misc.py
+++ Yuriko/modules/misc.py
@@ -226,7 +226,7 @@ __help__ = """
 
 ✗ /lyrics (song name) - `This plugin searches for song lyrics with song name.`
 
-*✗ 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 💕 𝐁𝐲: 𝗗𝗲𝘃𝗶𝗟 𝗛𝗮𝗰𝗸𝗲𝗥 @JaiHindChatting !*
+*✗ Powered 💕 By: Mastermind Network(MNO) @mastermind_network_official !*
 """
 
 ECHO_HANDLER = DisableAbleCommandHandler(
